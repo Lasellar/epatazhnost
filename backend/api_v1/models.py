788730974667d6878be3557957c45c6d5e2db4c7
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-19 23:08:39 +0300

    models base

--- backend/api_v1/models.py
+++ backend/api_v1/models.py
@@ -1,3 +1,34 @@
-from django.db import models
+from django.db.models import (
+    Model, CharField, IntegerField, ForeignKey, BooleanField,
+    ManyToManyField, SlugField, TextField, CASCADE, ImageField
+)
 
-# Create your models here.
+
+class Category(Model):
+    name = CharField(max_length=30, unique=True)
+    slug = SlugField(max_length=50, unique=True)
+
+
+class Size(Model):
+    name = CharField(max_length=10, unique=True)
+
+
+class Item(Model):
+    name = CharField(max_length=64)
+    description = TextField()
+    sizes = ManyToManyField(Size, related_name='itemsizes', through='ItemSize')
+    is_published = BooleanField(blank=True, default=False)
+    main_image = ImageField(upload_to='items_images')
+
+
+class ItemSize(Model):
+    item = ForeignKey(Item, on_delete=CASCADE, related_name='itemsize')
+    size = ForeignKey(Size, on_delete=CASCADE, related_name='itemsize')
+
+
+class ImageItem(Model):
+    image = ImageField(upload_to='items_images')
+    item = ForeignKey(
+        Item, related_name='imageitem', on_delete=CASCADE
+    )
+    is_published = BooleanField(blank=True, default=False)
