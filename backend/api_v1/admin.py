f95f6ce0a266362e667aa371a7b8c31dcf50f475
Maksim Polushin <btnchsky.sad@mail.ru>
2025-01-22 03:06:25 +0300

    admin pannel changes

--- backend/api_v1/admin.py
+++ backend/api_v1/admin.py
@@ -25,14 +25,18 @@ class ImageInline(TabularInline):
 @register(Category)
 class CategoryAdmin(CustomModelAdmin):
     list_display = ('id', 'name', 'slug')
+    list_display_links = ('name',)
 
 
 @register(Size)
 class SizeAdmin(CustomModelAdmin):
     list_display = ('id', 'name')
+    list_editable = ('name',)
 
 
 @register(Item)
 class ItemAdmin(CustomModelAdmin):
     inlines = (SizeInline, ImageInline)
     list_display = ('id', 'name', 'is_published')
+    list_display_links = ('name',)
+    list_editable = ('is_published', )
