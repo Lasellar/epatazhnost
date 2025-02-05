49d43f8ea933b155fd7aebed45dfaf4c9000f32e
Maksim Polushin <btnchsky.sad@mail.ru>
2025-02-05 17:36:09 +0300

    YET ANOTHER CSRF_TRUSTED_ORIGINS FIX

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -119,4 +119,7 @@ CORS_URLS_REGEX = r'^/api_v1/.*$'
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:3000',
 ]
-CSRF_TRUSTED_ORIGINS = ['https://epatazhnost.ddns.net/']
+CSRF_TRUSTED_ORIGINS = [
+    'https://epatazhnost.ddns.net',
+    'http://epatazhnost.ddns.net',
+]
