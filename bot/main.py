be0c4579351854e4461d3b1c329115194ff5aaca
Maksim Polushin <btnchsky.sad@mail.ru>
2025-02-07 17:56:21 +0300

    bot start logic

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ from pyrogram import Client, filters
 from pyrogram.types import Message
 from dotenv import load_dotenv
 
+from texts import hi
+
 from os import getenv
 from datetime import datetime
 
@@ -14,10 +16,21 @@ ME = getenv('ME')
 bot = Client('bot', bot_token=TOKEN, api_hash=API_HASH, api_id=API_ID)
 
 
-print(datetime.now())
+@bot.on_message(~filters.group)
+async def echo(client_object, message: Message):
+    await message.reply(f'[{datetime.now()}] {message.text}')
+
+
+@bot.on_message(filters.command('start') & ~filters.group)
+async def start_command(client_object, message: Message):
+    await bot.send_message(
+        chat_id=message.chat.id,
+        text=hi
+    )
+
+
 while True:
     try:
         bot.run()
     except Exception as ex:
         print(ex)
-
