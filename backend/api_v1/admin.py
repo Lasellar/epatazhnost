f4d1a64315c29b6d401af58b58223a34f717f0d6
Maksim Polushin <btnchsky.sad@mail.ru>
2025-02-07 14:14:19 +0300

    MainPagePhotoAdmin

--- backend/api_v1/admin.py
+++ backend/api_v1/admin.py
@@ -4,7 +4,8 @@ from django.contrib.admin import (
 )
 
 from .models import (
-    Category, Size, ItemSize, Item, ImageItem, Gig, ShoppingCart
+    Category, Size, ItemSize, Item, ImageItem, Gig,
+    ShoppingCart, MainPagePhoto
 )
 
 
@@ -54,3 +55,9 @@ class GigAdmin(CustomModelAdmin):
 @register(ShoppingCart)
 class ShoppingCartAdmin(CustomModelAdmin):
     list_display = ('user_cookie', 'item')
+
+
+@register(MainPagePhoto)
+class MainPagePhotoAdmin(CustomModelAdmin):
+    list_display = ('id', 'image', 'is_published')
+    list_editable = ('is_published',)
